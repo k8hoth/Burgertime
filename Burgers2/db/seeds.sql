70e299365f3375690ab6c62f23ca657312ed4708
Katie88 <kathrynhoth@gmail.com>
2020-02-01 14:18:12 -0700

    adding working files from pc

---
+++ Burgers2/db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Green Chile Cheeseburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Turkeyburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Black Bean');
+INSERT INTO burgers
+    (name, devoured)
+VALUES('Bacon', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES('Jalapeno', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES('Chili', true);
